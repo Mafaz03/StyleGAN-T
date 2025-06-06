e3af79e491dc8d12b1bac63ec4413b3c44c826ad
Mafaz03 <mohdmafaz200303@gmail.com>
2025-06-06 09:59:01 +0530

    is_running_on_kaggle

--- Network/config.py
+++ Network/config.py
@@ -1,3 +1,8 @@
+import os
+
+def is_running_on_kaggle():
+    return "KAGGLE_KERNEL_RUN_TYPE" in os.environ
+
 Z_DIM          = 64
 C_DIM          = 768
 BATCH_SIZE     = 1
@@ -6,4 +11,6 @@ DEVICE         = "cpu"
 IMG_RESOLUTION = 128
 CLIP_wWEIGHT   = 0.2
 BLUR_FADE_KIMG = 0.1 # after 100 image there will be 0 Blur,
-DATASET_PATH   = "/Users/mohamedmafaz/Desktop/StyleGAN-T/notebooks/Networks/dataset/"
+
+KAGGLE_STR = "/kaggle/working/StyleGAN-T/" if is_running_on_kaggle() else ""
+DATASET_PATH   = f"{KAGGLE_STR}/notebooks/Networks/dataset/"
